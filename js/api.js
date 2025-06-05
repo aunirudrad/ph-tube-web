7c5cd2e86a6d4cd7789280896c9aa2be5ab57e33
aunirudrad <anu15-4170@diu.edu.bd>
2025-06-06 03:45:24 +0600

    API fetched

--- js/api.js
+++ js/api.js
@@ -9,7 +9,7 @@ function loadCategories() {
 
 function displayCategories(categories) {
 
-    console.log(categories);
+    // console.log(categories);
 
     const dynamicCategories = document.getElementById("categories");
 
@@ -26,4 +26,47 @@ function displayCategories(categories) {
 
 }
 
+
+const loadVideos = () => {
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+        .then(res => res.json())
+        .then(data => displayVideos(data.videos));
+}
+
+const displayVideos = (videos) => {
+    // console.log(videos);
+    const videoContainer = document.getElementById("videos");
+
+    videos.forEach(video => {
+        // console.log(video);
+
+        const singleVideo = document.createElement("div");
+        singleVideo.innerHTML = `
+        <div class="card bg-base-100">
+            <figure class="rounded-lg h-[200px] relative">
+                <img class="w-full h-[200px] object-cover" src="${video.thumbnail}" alt="Shoes" />
+                <span class="absolute text-white text-[0.625rem] bottom-2 right-2 bg-black py-1 px-2 rounded">3hrs 56 min ago</span>
+            </figure>
+            <div class="flex gap-4 pt-5">
+                <div class="">
+                    <img class="rounded-full w-10 h-10" src="${video.authors[0].profile_picture}" alt="">
+                </div>
+                <div>
+                    <h2 class="font-bold text-text-secondary">${video.title}</h2>
+                    <p class="text-sm py-2 text-text-ternary flex gap-3 items-center">${video.authors[0].profile_name} <img class="w-5" src="https://img.icons8.com/?size=64&id=eZo3c88c63il&format=png"></p>
+                    <p class="text-sm text-text-ternary">${video.others.views} Views</p>
+                </div>
+
+            </div>
+        </div>
+        `;
+
+        videoContainer.appendChild(singleVideo);
+
+    });
+
+};
+
+
 loadCategories();
+loadVideos();
